23606e2344822cc7ee96d01497189bb96c9a371c
FranciscoRZ <franciscoreyzaluar@gmail.com>
2020-04-23 12:34:06 +0200

    implement M. G. SURE thresholding

--- production_code/denoising.py
+++ production_code/denoising.py
@@ -1,10 +1,12 @@
 import abc
 import numpy as np
+import scipy.optimize as spo
+
 
 class __AbstractDenoiser(abc.ABC):
 
     #region UNIVERSAL_THRESHOLD
-    def __estimate_noise_variance(self, WT: dict) -> float:
+    def estimate_noise_variance(self, WT: dict) -> float:
         max_scale = max([x[0] for x in WT.keys()])
         wavelet_coeffs_max_scale = [v for (k, v) in WT.items() if k[0]==max_scale]
 
@@ -18,7 +20,7 @@ class __AbstractDenoiser(abc.ABC):
         return 2 * max_scale
 
     def compute_universal_threshold(self, WT: dict) -> float:
-        noise_variance = self.__estimate_noise_variance(WT)
+        noise_variance = self.estimate_noise_variance(WT)
         T = self.__get_signal_length(WT)
         return pow(2 * np.log(T), 0.5) * noise_variance
     #endregion
@@ -51,6 +53,7 @@ class __AbstractDenoiser(abc.ABC):
         return WT_denoised
     #endregion
 
+    
 class SURE(__AbstractDenoiser):
 
     def __init__(self):
@@ -81,6 +84,48 @@ class SURE(__AbstractDenoiser):
         min_idx = np.argmin(all_SURE_scores)
         return all_thresholds[min_idx]
 
+    
+class SURE_G(__AbstractDenoiser):
+    ''' Implements SURE denoising as specified in page 120 of M. G. course material
+    '''
+    
+    def __init__(self):
+        super()
+        pass
+    
+    def __compute_SURE_SI(self, y: float, threshold: float, sigma: float) -> float:
+        ''' Implements SI function in SURE sum page 120 M. G. course material.
+        '''
+        if abs(y) >= threshold:
+            return pow(threshold, 2) + pow(sigma, 2)
+        return pow(y, 2) - pow(sigma, 2)
+     
+    def __compute_SURE(self, coeffs:list, threshold:float, sigma:float) -> float:
+        ''' Implements SURE estimation of wavelet reconstruction error as in page 120 of M.G course material.
+        '''
+        return np.sum([self.__compute_SURE_SI(x, sigma, threshold) for x in coeffs])
+    
+    def compute_threshold(self, coeffs:list, univ_threshold:float, sigma:float) -> float:
+        ''' Computes the SURE threshold as specified page 119 in M. G. course material.
+        '''
+        res = spo.minimize(lambda x: self.__compute_SURE(coeffs, x, sigma), x0=univ_threshold)
+        return res.x[0]
+
+    def denoise_coefficients(self, WT: dict, method: str='hard') -> dict:
+        ''' Override mother method because of "T" which we don't use anymore
+        '''
+        all_scales = set([k[0] for (k, v) in WT.items()])
+        sigma_estimate = self.estimate_noise_variance(WT) 
+        WT_denoised = dict()
+        universal_threshold = self.compute_universal_threshold(WT)
+        for scale in all_scales:
+            scale_coefficients = [v for (k, v) in WT.items() if k[0]==scale]
+            threshold = self.compute_threshold(scale_coefficients, universal_threshold, sigma_estimate)
+            scale_coefficients = self.suppress_by_scale(scale_coefficients,
+                                                        threshold, method)
+            WT_denoised.update({(scale, i):val for (i, val) in enumerate(scale_coefficients)})
+        return WT_denoised
+    
 class SUREShrink(SURE):
 
     def __init__(self):
@@ -101,3 +146,4 @@ class SUREShrink(SURE):
             return universal_threshold
         else:
             return super().compute_threshold(ts, universal_threshold, T)
+
